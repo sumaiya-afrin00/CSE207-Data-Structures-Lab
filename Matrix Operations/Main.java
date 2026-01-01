dc4ff7fdd77352d642f00b14c6fb5bd1e33b664d
Sumaiya Afrin <sumaiyaafrin0036@gmail.com>
2026-01-01 22:50:05 +0600

    Add files via upload

---
+++ Matrix Operations/Main.java
@@ -0,0 +1,34 @@
+package labTask3;
+
+import java.util.Scanner;
+
+public interface Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the size of row: ");
+		int row = input.nextInt();
+		
+		System.out.println("Enter the size of col: ");
+		int col = input.nextInt();
+		
+		int [][]A = new int [row] [col];
+		int [][]B = new int [row] [col];
+		
+		System.out.println("Enter A: ");
+		for(int i = 0; i<row; i++) {
+			for(int j = 0; j<col; j++) {
+				A[i][j] = input.nextInt();
+			}
+		}
+		
+		System.out.println("Enter B: ");
+		for(int i = 0; i<row; i++) {
+			for(int j = 0; j<col; j++) {
+				B[i][j] = input.nextInt();
+			}
+		}
+		
+		
+	}
+
+}
